4abb58f4cdec2aa628bf28a82fbca554abe90e2a
Tommy Seternus <thomassseternus@gmail.com>
2024-11-24 16:21:53 -0500

    Got part 3 of tutorial complete
    Math quiz works and checks if the answers are correct, and has a timer of 30 seconds, if not finished in time, program gives you the answers

--- MathQuiz/Form1.cs
+++ MathQuiz/Form1.cs
@@ -35,6 +35,10 @@ namespace MathQuiz
         // for the division problem. 
         int dividend;
         int divisor;
+
+        // This integer variable keeps track of the 
+        // remaining time.
+        int timeLeft;
         public Form1()
         {
             InitializeComponent();
@@ -84,12 +88,68 @@ namespace MathQuiz
             dividedLeftLabel.Text = dividend.ToString();
             dividedRightLabel.Text = divisor.ToString();
             Quotient.Value = 0;
+
+            // Start the timer.
+            timeLeft = 30;
+            timeLabel.Text = "30 seconds";
+            timer1.Start();
         }
 
         private void startButton_Click(object sender, EventArgs e)
         {
             StartTheQuiz();
             startButton.Enabled = false;
+        }
+
+        /// <summary>
+        /// Check the answers to see if the user got everything right.
+        /// </summary>
+        /// <returns>True if the answer's correct, false otherwise.</returns>
+        private bool CheckTheAnswer()
+        {
+            if ((addend1 + addend2 == Sum.Value)
+                && (minuend - subtrahend == Difference.Value)
+                && (multiplicand * multiplier == Product.Value)
+                && (dividend / divisor == Quotient.Value))
+                return true;
+            else
+                return false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (CheckTheAnswer())
+            {
+                // If CheckTheAnswer() returns true, then the user 
+                // got the answer right. Stop the timer  
+                // and show a MessageBox.
+                timer1.Stop();
+                MessageBox.Show("You got all the answers right!",
+                                "Congratulations!");
+                startButton.Enabled = true;
+            }
+            else if (timeLeft > 0)
+            {
+                // If CheckTheAnswer() returns false, keep counting
+                // down. Decrease the time left by one second and 
+                // display the new time left by updating the 
+                // Time Left label.
+                timeLeft = timeLeft - 1;
+                timeLabel.Text = timeLeft + " seconds";
+            }
+            else
+            {
+                // If the user ran out of time, stop the timer, show
+                // a MessageBox, and fill in the answers.
+                timer1.Stop();
+                timeLabel.Text = "Time's up!";
+                MessageBox.Show("You didn't finish in time.", "Sorry!");
+                Sum.Value = addend1 + addend2;
+                Difference.Value = minuend - subtrahend;
+                Product.Value = multiplicand * multiplier;
+                Quotient.Value = dividend / divisor;
+                startButton.Enabled = true;
+            }
         }
     }
 }
